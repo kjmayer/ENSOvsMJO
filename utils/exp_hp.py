7e088f6554390decbef81eb5af8fbffb9781a077
“kjmayer” <kjmayer@ucar.edu>
2023-08-24 12:40:28 -0600

    updating scripts to train and analyze at different leads for NDJF(M)

--- utils/exp_hp.py
+++ utils/exp_hp.py
@@ -20,6 +20,24 @@ def get_hp(experiment_name):
             'GLOBAL_SEED': 99,
         },
         
+        ## Experiment 1:
+        'exp2': {
+            'INPUT': ['TS_SST_ONI','RMM1_CESM2','RMM2_CESM2'],
+            'OUTPUT': ['TS_Z500a'],
+            'LEAD': 14,
+            'X_ADDITIONAL_DAYS': 15,
+            'Y_RUNMEAN': 7,
+            'HIDDENS1': [8], 
+            'HIDDENS2': [8],
+            'RIDGE1': 0,
+            'RIDGE2': 0,
+            'BATCH_SIZE': 32,
+            'LR': 0.001,
+            'DROPOUT_RATE': 0,
+            'PATIENCE': 20,
+            'GLOBAL_SEED': 99,
+        },
+        
     }
 
     return experiments[experiment_name]
